f4355221bb0fcbfc1338bfd42ef1962095181c22
Dimitar Dimitrov <daghostman.dd@gmail.com>
2022-11-06 02:01:58 +0200

    add minimal confiuration for phpunit

---
+++ tests/DummyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use Onion\Framework\Test\TestCase;
+
+class DummyTest extends TestCase
+{
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+}
